75746e9d1b90727f6de46178ca627d518507968e
Félix Pereira <felixpereira538@gmail.com>
2021-12-13 13:31:21 +0200

    Adding the hero image

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -4,8 +4,14 @@ import './hero.css';
 const hero = () => {
     return(
         <section className="hero" id="hero">
-            <div className="hero__img">
-                <img src='images/bg-intro-mobile.svg' alt='bg-intro-mobile' />
+            <div style={{
+                backgroundImage: 'url("images/bg-intro-desktop.svg")',
+                backgroundPosition: 'center',
+                backgroundSize: '830px 900px',
+                backgroundRepeat: 'no-repeat'
+                }}
+                 className="hero__img">
+                <img src='images/image-mockups.png' alt='bg-intro-mobile' />
             </div>
             <div className="hero__text">
                 <h2 className="hero__title">Next generation digital banking</h2>
